050b318442565a7437a33e97aa3c6627d53460dd
CasLinden <cas_linden@msn.com>
2023-07-14 23:29:08 +0900

    added functionality to write a message to the message board

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('loading message board')
+  res.render('index', { title: 'Message Board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+  console.log("Inside /new route handler");
+});
+
+router.post('/new', function(req, res, next){
+  console.log(req.body)
+  messages.push({text: req.body.message, user: req.body.user, added: new Date()});
+  res.redirect('/')
 });
 
 module.exports = router;
